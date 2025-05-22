77be6edd04e4713204670b0d3e6d803fcbd813bc
jglancy95 <jasmine.glancy.vt@gmail.com>
2025-05-22 01:31:36 -0400

    feat: add favicon

--- author-stats/src/app/login/page.tsx
+++ author-stats/src/app/login/page.tsx
@@ -1,21 +1,39 @@
 import Header from "@/components/header";
+import Footer from "@/components/footer";
+import Input from "@/components/input";
 
 export default function login() {
     
     return (
         <div>
             <main className="flex gap-[32px] row-start-2 items-center sm:items-start">
-            <div className="bg-[var(--dove_gray)] border-5 border-double border-[var(--tropical_indigo)] h-fit-content w-[65em] pt-10 text-[var(--dark_purple)] rounded-[2em]">
-                <Header />
-                <div className="flex justify-center">
-                    <fieldset className="fieldset">
-                        <legend className="fieldset-legend">What is your name?</legend>
-                        <input type="text" className="input" placeholder="Type here" />
-                        <p className="label">Optional</p>
-                    </fieldset>
+                <div className="bg-[var(--dove_gray)] border-5 border-double border-[var(--tropical_indigo)] h-fit-content w-[65em] pt-10 text-[var(--dark_purple)] rounded-[2em]">
+                    <Header />
+                    <div className="flex flex-col items-center justify-center pb-5">
+                        <h2 className="font-bold pb-5">Log In</h2>
+
+                        <Input isUsername={true} validator={
+                            <>
+                            Must be 3 to 30 characters
+                            <br />
+                            containing only letters, numbers or dash
+                            </>
+                        } placeholder="Username" />
+                        <Input isPassword={true} validator={
+                            <>
+                                Must be more than 8 characters, including:
+                                <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+                            </>
+                        } placeholder="Password" />
+                    </div>
+                    <div className="flex justify-center">
+                        <button className="btn btn-soft btn-primary text-[var(--tropical_indigo)] hover:text-[var(--dark_purple)] hover:bg-linear-to-t from-[var(--amethyst)] to-[var(--tropical_indigo)] btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl mb-10 mt-5">
+                            Login
+                        </button>
+                    </div>
                 </div>
-            </div>
             </main>
+            <Footer />
         </div>
     );
 }
--- author-stats/src/components/input.tsx
+++ author-stats/src/components/input.tsx
@@ -1,5 +1,7 @@
+"use client";
 
 import { DayPicker } from "react-day-picker";
+
 
 type inputTypes = {
     fieldLegend: string;
@@ -7,9 +9,11 @@ type inputTypes = {
     isOptional?: boolean;
     isNumber?: boolean;
     isName?: boolean;
-    validator?: string;
+    validator?: string | Element;
     isDate?: boolean;
     date?: Date;
+    isUsername?: boolean;
+    isPassword?: string;
     setDate?: (date: Date | undefined) => void;
     pickerOpen?: boolean;
     setPickerOpen?: (open: boolean) => void;
@@ -17,8 +21,9 @@ type inputTypes = {
 
 export default function input({ fieldLegend, placeholder, isOptional, 
                                 isNumber, isName, validator, 
-                                isDate, date, setDate, 
-                                pickerOpen, setPickerOpen }: inputTypes) {
+                                isDate, date, isUsername,
+                                isPassword, setDate, pickerOpen, 
+                                setPickerOpen }: inputTypes) {
     return(
         <fieldset className="fieldset">
             {isNumber ? (
@@ -73,6 +78,67 @@ export default function input({ fieldLegend, placeholder, isOptional,
                             </div>
                         )}
                         <p className="validator-hint">{validator}</p>
+                    </>
+                ) : isUsername? (
+                    <>
+                    <label className="input validator">
+                        <svg className="h-[1.5em] opacity-50 text-[var(--tropical_indigo)]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                            strokeLinejoin="round"
+                            strokeLinecap="round"
+                            strokeWidth="2.5"
+                            fill="none"
+                            stroke="currentColor"
+                            >
+                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="12" cy="7" r="4"></circle>
+                            </g>
+                        </svg>
+                        <input
+                            type="text"
+                            required
+                            placeholder={placeholder}
+                            pattern="[A-Za-z][A-Za-z0-9\-]*"
+                            minLength={3}
+                            maxLength={30}
+                            className="w-[15em] text-center"
+                            title="Only letters, numbers or dash"
+                        />
+                        </label>
+                        <p className="validator-hint pb-2">
+                            {validator}
+                        </p>
+                    </>
+                ) : isPassword ? (
+                    <>
+                        <label className="input validator">
+                        <svg className="h-[1.5em] opacity-50 text-[var(--tropical_indigo)]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                            strokeLinejoin="round"
+                            strokeLinecap="round"
+                            strokeWidth="2.5"
+                            fill="none"
+                            stroke="currentColor"
+                            >
+                            <path
+                                d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+                            ></path>
+                            <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                            </g>
+                        </svg>
+                        <input
+                            type="password"
+                            required
+                            placeholder={placeholder}
+                            minLength="8"
+                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                            className="w-[15em] text-center"
+                            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                        />
+                        </label>
+                        <p className="validator-hint hidden">
+                            {validator}
+                        </p>
                     </>
                 ) : ( 
                     <>
